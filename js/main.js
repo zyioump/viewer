1f9a05111b0091f4171894106a92c91e8492fc0a
Simon Archieri <simon@archieri.fr>
2017-07-12 19:26:16 +0200

    Setting are now available

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-window.db = "http://opv_master:5000";
-window.fm = "http://opv_master:5050";
-window.campaign = 1;
-window.cul = 1;
+function go(){
+  window.db = document.getElementById("db").value;
+  window.fm = document.getElementById("fm").value;
+  window.campaign = document.getElementById("campaign").value;
+  window.cul = document.getElementById("cul").value;
 
-var lotService = LotService();
+  var lotService = LotService();
 
-var mapManager = MapManager();
-mapManager.initMap();
-mapManager.displayMap(lotService);
+  var mapManager = MapManager();
+  mapManager.initMap();
+  mapManager.displayMap(lotService);
 
-var pannellumConfig = PannellumConfig();
-pannellumConfig.makeConfigScene(lotService);
-var sceneConfig = pannellumConfig.getSceneConfig();
+  var pannellumConfig = PannellumConfig();
+  pannellumConfig.makeConfigScene(lotService);
+  var sceneConfig = pannellumConfig.getSceneConfig();
 
-// -- loading pannellums
-var viewers = ViewersManagers();
-var pt, pf;
-viewers.initViewers(sceneConfig, function(){ alert("inited");});
-pt = viewers.getPanTo();
-pf = viewers.getPanFrom();
+  // -- loading pannellums
+  var viewers = ViewersManagers();
+  var pt, pf;
+  viewers.initViewers(sceneConfig, function(){ alert("inited");});
+  pt = viewers.getPanTo();
+  pf = viewers.getPanFrom();
+}
 
 // dirty event manager
 var loadPanorama = function(sceneId, viewerId){
@@ -44,6 +46,7 @@ function activate(id_lot, id_campaign, id_malette){
     mapManager.displayMap(lotService);
   });
 }
+
 function desactivate(id_lot, id_campaign, id_malette){
   lot = lotService.getLot(id_campaign, id_lot, function(lot){
     lot.active = null;
